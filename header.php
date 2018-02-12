a9dda52b5f74284715a15ff2872af4281267af33
James Loveridge <dev@j-m.es>
2018-02-12 00:11:29 +0000

    Add header wp_nav_menu()

--- header.php
+++ header.php
@@ -8,7 +8,19 @@
 	<body <?php body_class(); ?>>
 
 		<header class="header" role="banner">
-		 
+		 	<h1 class="header-title"><?php echo the_title(); ?></h1>
+
+			<?php
+				// https://developer.wordpress.org/reference/functions/wp_nav_menu/
+				$args = array(
+					'container' => 'nav',
+					'container_class' => 'header-menu',
+					'fallback_cb' => 'header',
+					'menu' => 'header',
+					'theme_location' => 'header'
+				);
+				wp_nav_menu( $args );
+			?>
 		</header>
 
 		<main <?php post_class('main'); ?> role="main">
