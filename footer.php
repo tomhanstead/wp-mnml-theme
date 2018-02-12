e479773f00ce37fbc6b8999a17480e3e820ecafb
James Loveridge <dev@j-m.es>
2018-02-12 00:11:43 +0000

    Add footer wp_nav_menu()

--- footer.php
+++ footer.php
@@ -2,7 +2,17 @@
 		</main>
 
 		<footer class="footer" role="contentinfo">
-
+			<?php
+				// https://developer.wordpress.org/reference/functions/wp_nav_menu/
+				$args = array(
+					'container' => 'nav',
+					'container_class' => 'footer-menu',
+					'fallback_cb' => 'header',
+					'menu' => 'footer',
+					'theme_location' => 'footer'
+				);
+				wp_nav_menu( $args );
+			?>
 		</footer>
 
 	</body>
